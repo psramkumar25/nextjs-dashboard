7aa34f6b53f14fc9298518e7d9d105f7287808a0
Ram Kumar <psramkumar25@gmail.com>
2023-12-29 18:48:38 +0530

    chore: :loud_sound: enable logs in Dev Server

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // generateBuildId: async () => process.env.GIT_HASH,
+  logging: { fetches: { fullUrl: true } },
+  // output: 'standalone',
+};
 
 module.exports = nextConfig;
