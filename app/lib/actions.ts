f0c2f7f7ae0ac391288cd4d4879494d97cc2055b
Ram Kumar <psramkumar25@gmail.com>
2023-12-30 23:54:32 +0530

    feat: Chapter 14: Improving accessibility
    Add Form Validation in Create Invoice Form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,24 +6,53 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    required_error: 'Please select a customer.',
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    required_error: 'Please select an invoice status.',
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type FormState = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: FormState, formData: FormData) {
+  // Validate formdata using Zod
+  const rawFormData = Object.fromEntries(formData.entries());
+  const invoiceData = CreateInvoice.safeParse(rawFormData);
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!invoiceData.success) {
+    return {
+      errors: invoiceData.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare Data for insertion into database
+  const { customerId, amount, status } = invoiceData.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // throw new Error('Failed to Create Invoice');
+  // Insert into database
   try {
-    const rawFormData = Object.fromEntries(formData.entries());
-    const invoiceData = CreateInvoice.parse(rawFormData);
-    const { customerId, amount, status } = invoiceData;
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    
-    // throw new Error('Failed to Create Invoice');
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
@@ -31,7 +60,7 @@ export async function createInvoice(formData: FormData) {
   } catch (error) {
     console.error(error);
     throw error;
-    return { message: 'Database Error: Failed to Create Invoice.' };
+    // return { message: 'Database Error: Failed to Create Invoice.' };
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
